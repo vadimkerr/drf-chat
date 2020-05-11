18e15fb54925cf0580214c3205b319bc30e22fb5
Vadim Stepanov <vadimkerr@gmail.com>
2020-05-11 17:56:19 +0300

    create message model

--- drf_chat/api/models.py
+++ drf_chat/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="receiver"
+    )
+    text = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now=True)
